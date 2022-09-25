67fc8eb99cdbf6307c141395f2cd7bd7263614eb
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-09-25 19:39:18 +0400

    Vue Forms

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  // data() {
+  //   return {
+  //     activeOption: this.modelValue,
+  //   };
+  // },
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: purple;
+}
+.active button {
+  color: purple;
+}
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solic #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: solid 1px grey;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
